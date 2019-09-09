bc5e9364492ed66c0d32778c372c6bdb9363af12
daninthemoon <mrcoolguy37@protonmail.com>
2019-09-09 14:32:58 -0700

    SHIT

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,9 @@
+from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+
+from lists.views import home_page
 
 # Create your tests here.
-class SmokeTest(TestCase):
-    """description"""
 
-    def test_bad_math(self):
-        self.assertEqual(1 + 1, 3)
         
